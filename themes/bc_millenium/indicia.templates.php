c1ea8af4f5ab93bebdfdd6bbff8442993ed11492
John van Breda <john.vanbreda@biodiverseit.co.uk>
2017-03-14 12:30:45 +0000

    Theme updates

---
+++ themes/bc_millenium/indicia.templates.php
@@ -0,0 +1,9 @@
+<?php
+global $indicia_templates;
+
+$indicia_templates['two-col-50'] = <<<TEMPLATE
+<div class="l-rw arc--2 hr--2-3">
+  <div class="l-r">{col-1}</div>
+  <div class="l-r">{col-2}</div>
+</div>
+TEMPLATE;
